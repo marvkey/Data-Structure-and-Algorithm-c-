e855aae2eeae6e909bc360aefa206e352ee6ab01
marvkey <marvnwadike1@gmail.com>
2021-12-22 18:10:35 -0800

    new heap operations

--- DataStructuresAlgorithms.Youtube/src/DSA/Trees/Heap.h
+++ DataStructuresAlgorithms.Youtube/src/DSA/Trees/Heap.h
@@ -1,6 +1,11 @@
 #pragma once
 #include<iostream>
 #include<math.h>
+void swap(int& x, int& y) {
+	int temp = x;
+	x = y;
+	y = temp;
+}
 class MinHeap {
 public:
 	int* Harr; // pointer to elements
@@ -31,5 +36,66 @@ public:
 		return ceil(log2(HeapSize + 1) - 1);
 		// ciel reuturns the smallest integer that is 
 		// greater or equal to x
+	}
+
+	int Parent(int index) {
+		return (index - 1) / 2;
+	}
+	int Left(int index) {
+		return (2 * index + 1);
+	}
+
+	int Right(int index) {
+		return (2 * index + 2);
+	}
+	void InsertKey(int k) {
+		if (HeapSize == Capicity) {
+			std::cout << "\nOverflow: Could not insert Key\n";
+			return;
+		}
+
+		// First insert the new key at the end
+		HeapSize++;
+		int i = HeapSize - 1;
+		Harr[i] = k;
+
+		// Fix the min heap property if it is violated
+		while (i != 0 && Harr[Parent(i)] > Harr[i]) {
+			swap(Harr[i], Harr[Parent(i)]);
+			i = Parent(i);
+		}
+	}
+
+	// Method to remove minimum element (or root) from min heap
+	int ExtractMin() {
+		if (HeapSize<= 0)
+			return INT_MAX;
+		if (HeapSize == 1) {
+			HeapSize--;
+			return Harr[0];
+		}
+
+		// Store the minimum value, and remove it from heap
+		int root = Harr[0];
+		Harr[0] = Harr[HeapSize- 1];
+		HeapSize--;
+		MinHeapify(0);
+
+		return root;
+	}
+	// A recursive method to heapify a subtree with the root at given index
+ // This method assumes that the subtrees are already heapified
+	void MinHeapify(int i) {
+		int l = Left(i);
+		int r = Right(i);
+		int smallest = i;
+		if (l < HeapSize && Harr[l] < Harr[i])
+			smallest = l;
+		if (r < HeapSize && Harr[r] < Harr[smallest])
+			smallest = r;
+		if (smallest != i) {
+			swap(Harr[i], Harr[smallest]);
+			MinHeapify(smallest);
+		}
 	}
 };
